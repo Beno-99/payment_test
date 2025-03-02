f03d8777a33b0a56f61c01bb423bd5aab0ef6b7e
Beno-99 <beniamin99king@gmail.com>
2025-03-02 17:58:15 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-sm  text-blue">Hello, Tailwind CSS!</h1>
+      </div>
+    </>
+  );
+};
+export default Home;
---
+++ src/app/payment/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const payment = () => {
+  return <div className="text-xl">payment</div>;
+};
+
+export default payment;
